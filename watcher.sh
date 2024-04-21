435435e91caf5cc62378f33b3fbe2cfc45ff9430
Darren Seet <35116035+bochap@users.noreply.github.com>
2024-04-21 14:28:14 +0000

    Week 3: dealing with toil Modify watcher.sh to monitor number of restarts to swpye-app and implement a kill switch to scale deployment to 0 when it exceeds maximum number of restarts

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,15 @@
-#TODO
+#!/bin/bash
+NAMESPACE="sre"
+DEPLOYMENT="swype-app"
+MAX_RESTARTS=4
+
+while true; do
+    NUM_RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+    echo "$DEPLOYMENT has $NUM_RESTARTS restarts"
+    if [ $NUM_RESTARTS -ge $MAX_RESTARTS ]; then
+        echo "$DEPLOYMENT has exceeded $((MAX_RESTARTS - 1)) restarts"
+        kubectl scale deployment ${DEPLOYMENT} -n ${NAMESPACE} --replicas=0
+        break
+    fi
+    sleep 60s
+done
